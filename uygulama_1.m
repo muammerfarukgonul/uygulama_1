4d08caf3e37bd02371b4979a8d6ae78d108f678b
muammerfarukgonul <44275790+muammerfarukgonul@users.noreply.github.com>
2018-10-24 21:25:27 +0300

    Revert "Revert "Update uygulama_1.m""
    This reverts commit 93b838d253f780df972a95ec7656b43289ab86a9.

---
+++ uygulama_1.m
@@ -0,0 +1,120 @@
+%***   written by muammer faruk gönül   ***
+% a þýkký
+a1=100;
+b=0.5;
+for i=1:375
+  
+    if i==1
+       result=a1;
+    else
+        
+        result=a1+(b*result);   
+    end
+    t(i)=result;
+end
+%b þýkký
+function y=fonksiyon(a,b,n)  % a=alfa b= beta n=gün sayýsýný belirtir.
+for i=1:3
+  
+    if i==1
+       y=a;
+    else
+        
+        y=a+(b*y);   
+    end
+end
+%c þýkký
+%recursive 
+function y=recursive(n) 
+a=100;
+b=0.5;
+if n<2
+    y=a;
+else
+   
+    y=a+(b*recursive(n-1));
+end
+end
+%iterative
+function y=fonksiyon(a,b,n)
+for i=1:3
+  
+    if i==1
+       y=a;
+    else
+        
+        y=a+(b*y);   
+        
+    end
+end
+%d þýkký
+% iki fonksiyonu ayý ayrý çalýþtýrdýðýmda computational time recursive =0.023609 hesaplarken
+% itarative =0.000003 hesaplanmýþtýr.
+function y=recursive(n)
+a=100;
+b=0.5;
+if n<2
+    y=a;
+else
+    tic;
+    y=a+(b*recursive(n-1));
+    toc;
+end
+end
+
+function y=fonksiyon(a,b,n)
+for i=1:3
+  
+    if i==1
+       y=a;
+    else
+        tic;
+        y=a+(b*y);   
+        toc;
+    end
+end
+% e þýkký
+a=1/4;
+b=3/5;
+for i=1:20
+    if i==1
+       result=a;
+        t(i)=result;
+    else
+        result=a+(b*result);   
+        t(i)=result;
+    end
+     
+end
+
+
+a1=1/2;
+b1=3/5;
+for j=1:20
+    if j==1
+       result1=a1;
+        t1(j)=result1;
+    else
+        result1=a1+(b1*result1);   
+        t1(j)=result1;
+    end
+     
+end
+
+
+a2=1/4;
+b2=3/4;
+for k=1:20
+    if k==1
+       result2=a2;
+        t2(k)=result2;
+    else
+        result2=a2+(b2*result2);   
+        t2(k)=result2;
+    end
+     
+end
+hold on
+stem (t,'o')
+stem(t1,'*')
+stem(t2,'x')
